7381133f7629f3e23b44b0e3a12401cb689eb1ad
EunSuGim <eunsu.kim3761@gmail.com>
2021-05-26 00:31:32 +0900

    <feat>: add categoriesRepositoryTest

---
+++ src/main/java/com/counchcoding/project/domain/categories/Categories.java
@@ -0,0 +1,31 @@
+package com.counchcoding.project.domain.categories;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Categories {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Builder
+    public Categories(String name, String code){
+        this.name = name;
+        this.code = code;
+    }
+
+
+}
---
+++ src/test/java/com/counchcoding/project/domain/categories/CategoriesRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.counchcoding.project.domain.categories;
+
+import com.counchcoding.project.Application;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class CategoriesRepositoryTest {
+
+    @Autowired
+    CategoriesRepository categoriesRepository;
+
+    @After
+    public void cleanUp(){
+        categoriesRepository.deleteAll();
+    }
+
+    @Test
+    public void load_categories(){
+        String name = "한식";
+        String code = "KOR";
+
+        categoriesRepository.save(Categories.builder()
+                .code(code)
+                .name(name)
+                .build());
+
+        List<Categories> categoriesList = categoriesRepository.findAll();
+
+        Categories categories = categoriesList.get(0);
+
+        assertThat(categories.getCode(), is(equalTo(code)));
+        assertThat(categories.getName(), is(equalTo(name)));
+
+
+
+
+    }
+}
